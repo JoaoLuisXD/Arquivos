c1aca589d798c017ec9df58dca675b46e919930c
João Luis <joaoluisd2005@gmail.com>
2025-04-24 14:36:03 -0300

    update

--- segundo_arquivo.c
+++ segundo_arquivo.c
@@ -1,22 +1,45 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <ctype.h>
 
 
 int main(int qtd_param, char* param[]){
  
-    if (qtd_param == 2){
+    if (qtd_param == 2)
+    {
         char* nome_arquivo = param[1];
 
-        FILE* fp = fopen(nome_arquivo, "rt");
+        FILE* fp = fopen(nome_arquivo, "rt"); // Lê o arquivo de texto "rt"
         
-        if(!fp){
+        if(!fp)
+        {
             printf("Erro ao abrir o arquivo: %s", nome_arquivo);
             exit(1);
         }
         
         printf("\nArquivo abriu com sucesso!\n");
 
+
+        int c = 0;
+        int nlinhas = 0;
+        while ((c = fgetc(fp)) != EOF) // Lê o arquivo, e atribui o valor em "c". Depois compara com "EOF"
+        {
+            if(c == '\n') // Se for igual '\n' entre aspas simples
+            {
+                nlinhas++; // Incrementa a quantidade de linhas
+            }
+        }
+        printf("Quantidade de linhas: %d",nlinhas+1);
+
+    
+
+
+
+
         fclose(fp);
     }
+    
+    
+
     return 0;
 }
