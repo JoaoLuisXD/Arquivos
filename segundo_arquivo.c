1c05f5e28006d282463ae70f5510eb8bdbbe108c
João Luis <joaoluisd2005@gmail.com>
2025-04-24 13:45:30 -0300

    update

---
+++ segundo_arquivo.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+
+int main(int qtd_param, char* param[]){
+ 
+    if (qtd_param == 2){
+        char* nome_arquivo = param[1];
+
+        FILE* fp = fopen(nome_arquivo, "rt");
+        
+        if(!fp){
+            printf("Erro ao abrir o arquivo: %s", nome_arquivo);
+            exit(1);
+        }
+        
+        printf("\nArquivo abriu com sucesso!\n");
+
+        fclose(fp);
+    }
+    return 0;
+}
