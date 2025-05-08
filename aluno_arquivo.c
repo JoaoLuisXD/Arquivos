be04bc70f79f0a2ac523b8ff985cf955c0abb34f
João Luis <joaoluisd2005@gmail.com>
2025-05-08 13:41:34 -0300

    update

---
+++ aluno_arquivo.c
@@ -0,0 +1,58 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+#define ARQUIVO_ENTRADA "aluno.txt"
+
+struct aluno
+{
+    int matricula;
+    char nome[101];
+    int idade;
+    char curso[101];
+};
+typedef struct aluno Aluno;
+
+
+
+int main()
+{
+    FILE * fp = fopen(ARQUIVO_ENTRADA,"rt");
+    if (fp == NULL)
+    {
+        exit(1);
+    }
+    
+    int c = 0;
+    int qtd_linhas = 0;
+    
+    
+    while ((c = fgetc(fp)) != EOF)
+    {
+        if(c == '\n')
+        {
+            qtd_linhas++;
+        }
+    }
+    Aluno * registro_aluno = (Aluno*)malloc(qtd_linhas*sizeof(Aluno));
+    rewind (fp);
+    for (int i = 0; i < qtd_linhas; i++)
+    {
+        fscanf(fp, "%d%*c ;",&registro_aluno[i].matricula);
+        fscanf(fp, "%s%*c ;",registro_aluno[i].nome);
+        fscanf(fp, "%d%*c ;",&registro_aluno[i].idade);
+        fscanf(fp, "%s%*c ;",registro_aluno[i].curso);
+    }
+    
+    for (int i = 0; i < qtd_linhas; i++)
+    {
+        printf("|%d| ",registro_aluno[i].matricula);
+        printf("|%s| ",registro_aluno[i].nome);
+        printf("|%d| ",registro_aluno[i].idade);
+        printf("|%s| ",registro_aluno[i].curso);
+        printf("\n");
+    }
+    
+    fclose(fp);
+    return 0;
+}
